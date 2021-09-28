d5dc35582b2b51b5ed0ae99094cd8120867b4343
vanshitamoolchandani <63742296+vanshitamoolchandani@users.noreply.github.com>
2021-09-29 03:47:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,39 +1,3 @@
-// var btnTranslate= document.querySelector("#button-translate");
-// var textInput = document.querySelector("#txt-input")
-// var output = document.querySelector("#output");
-
-// var url = "https://api.funtranslations.com/translate/groot.json"
-	
-
-// function getTranslationURL(text) {
-//     return url+ "?"+ "text="+text
-// }
-
-// function errorHandler(error){
-// console.log("error occured", error);
-// alert("something wrong with server! try again after some time. ")
-// }
-
-
-
-// function clickHandler() 
-// {
-//     var inputText = textInput.value;
-// fetch(getTranslationURL(inputText))
-// .then(response => response.json())
-// .then(json =>   {
-//                 var translatedText = json.contents.translatedText;
-//                 output.innerText = translatedText;
-//                 })
-// .catch(errorHandler)
-// };
-
-// btnTranslate.addEventListener("click", clickHandler)
-
-
-
-
-
 var buttonTranslate = document.querySelector("#button-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
